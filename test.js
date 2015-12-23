a8556e15dbd4d81d758f39208ad890c218f3f998
Peter Toth <email@petertoth.sk>
2015-12-23 16:37:55 +0100

    Replaced blokchain.info blockchain explorer with a stub

--- test.js
+++ test.js
@@ -1,14 +1,42 @@
 var chai = require('chai');
     expect = chai.expect,
-    chaiAsPromised = require('chai-as-promised');
+    chaiAsPromised = require('chai-as-promised'),
+    sinon = require('sinon'),
+    blockExplorer = require('blockchain.info/blockexplorer');
 
 chai.use(chaiAsPromised);
 
 var BitcoinTransactionPrice = require('./index');
 
 describe('Get price function', function() {
+  var sandbox, blockExplorerStub;
+
+  beforeEach(function(){
+    sandbox = sinon.sandbox.create();
+    blockExplorerStub = sandbox.stub(blockExplorer, 'getTx');
+  });
+
+  afterEach(function(){
+    sandbox.restore();
+  });
+
   it('Should return value of transaction', function(done) {
+   var blockExplorerStubResolveValue = {
+      inputs:[{
+         prev_out: {value: 76521777}
+      }],
+      out: [
+        {
+          value: 74520777,
+        }, {
+          value: 2000000,
+        }
+      ]
+    };
+
+    blockExplorerStub.returns(Promise.resolve(blockExplorerStubResolveValue));
     bitcoinTransactionPrice = new BitcoinTransactionPrice();
+
     bitcoinTransactionPrice.getPrice(95035819).then(function(obj) {
       expect(obj.inputs).to.deep.equal([0.76521777]);
       expect(obj.outputs).to.deep.equal([0.74520777, 0.02]);
