30b790010238fd6af70e4e82aa6a18f84cb276bc
Peter Toth <email@petertoth.sk>
2015-12-25 13:50:15 +0100

    Simplified assertion added catch(done) for failing tests

--- test.js
+++ test.js
@@ -4,8 +4,6 @@ var chai = require('chai');
     sinon = require('sinon'),
     blockExplorer = require('blockchain.info/blockexplorer');
 
-chai.use(chaiAsPromised);
-
 var BitcoinTransactionPrice = require('./index');
 
 describe('Get price function', function() {
@@ -35,19 +33,24 @@ describe('Get price function', function() {
       time: 1437205525
     };
 
+    var expectedResult = {
+      inputs: [ 0.76521777 ],
+      outputs: [ 0.74520777, 0.02 ],
+      fee: 0.00001,
+      inputsInFiat: [ 210.05 ],
+      outputsFiat: [ 204.55, 5.49 ],
+      feeFiat: 0.0027449380000000006,
+      bitcoinPriceFiat: 274.4938,
+      currency: 'USD'
+    }
+
+
     blockExplorerStub.returns(Promise.resolve(blockExplorerStubResolveValue));
     bitcoinTransactionPrice = new BitcoinTransactionPrice();
 
-    bitcoinTransactionPrice.getPrice(95035819).then(function(obj) {
-      expect(obj.inputs).to.deep.equal([0.76521777]);
-      expect(obj.outputs).to.deep.equal([0.74520777, 0.02]);
-      expect(obj.fee).to.equal(0.00001);
-      expect(obj.inputsInFiat).to.deep.equal([211.360226]);
-      expect(obj.outputsFiat).to.deep.equal([204.55, 5.49]);
-      expect(obj.feeFiat).to.equal(0.0027449380000000006);
-      expect(obj.bitcoinPriceFiat).to.equal(274.4938);
-      expect(obj.currency).to.equal('USD');
+    return bitcoinTransactionPrice.getPrice(95035819).then(function(obj) {
+      expect(obj).to.deep.equal(expectedResult);
       done();
-    });
+    }).catch(done);
   });
 });
